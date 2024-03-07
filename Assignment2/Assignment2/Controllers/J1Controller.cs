a1a127a31a9f89b06669546a4481f491f810b1ba
Ganga Suresh <gsk.gangasuresh@gmail.com>
2024-03-07 16:37:56 -0500

    assignment2

---
+++ Assignment2/Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2_j1.Controllers
+{
+    /// <summary>
+    /// recieves oders from user and calculate the total calories of the food
+    /// the arrays calorieb, caloried, calories, calorieds store the calories of the 4 menu options  of burger, drinks,side and desserts respectively from which item will be chosen as per user input and then added to get total calories
+    /// </summary>
+    /// <param name="burger">stores index of burger menu</param>
+    /// <param name="drink">stores index of drink menu</param>
+    /// <param name="side">stores index of side menu</param>
+    /// <param name="dessert">stores index of dessert menu</param>
+    /// <returns>
+    /// total calories after calculation.
+    /// 
+    /// </returns>
+    public class J1Controller : ApiController
+    {
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+
+        public int Menu(int burger, int drink, int side, int dessert)
+        {
+
+            int[] Calorieb = { 461, 431, 420, 0 };
+            int[] Caloried = { 130, 160, 118, 0 };
+            int[] Calories = { 100, 57, 70, 0 };
+            int[] Calorieds = { 167, 266, 75, 0 };
+
+            int totalCalories = Calorieb[burger - 1] + Caloried[drink - 1] + Calories[side - 1] + Calorieds[dessert - 1];
+
+            return totalCalories;
+        }
+    }
+
+}
---
+++ Assignment2/Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2_j1.Controllers
+{
+    /// <summary>
+    /// program uses for loop to check if sum of the numbers on both the dice adds up to 10; if yes the count increments by 1 forming the tootal number of chances to get 10
+    /// </summary>
+    /// <param name="m">receives the total side of one dice</param>
+    /// <param name="n">receives the total side of one dice</param>
+    /// <result>
+    /// gives total number of chances for geting sum =  10 from numbers obtained from 2 dices.
+    /// if count = 5 then outputs a string saying " there are 5 total ways to get the sum 10" 
+    /// </result>
+    public class J2Controller : ApiController
+    {
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string CalculateWaysToGetSum(int m, int n)
+        {
+
+            int count = 0;
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = n; j > 0; j--)
+                {
+                    if (i + j == 10)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 0)
+                return $"There are {count} ways to get the sum 10.";
+            else if (count == 1)
+                return $"There is {count} way to get the sum 10.";
+            else
+                return $"There are {count} total ways to get the sum 10.";
+        }
+    }
+}
